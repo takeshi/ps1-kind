e1f726cdadf08b5bbbb924042d12da763958b552
takeshi <takeshi.kondo@gmail.com>
2020-11-23 12:14:26 +0900

    Delete get-password.ps1

--- get-password.ps1
+++
@@ -1,5 +0,0 @@
-$b64txt = kubectl get secret mygitlab-gitlab-initial-root-password -ojsonpath='{.data.password}'
-$byte = [System.Convert]::FromBase64String($b64txt)
-$txt = [System.Text.Encoding]::Default.GetString($byte)
-# echo $b64txt
-echo $txt
